6690910a5be776aa4cacff62c3b8c868262c13ce
sathish kumar <sathishkumar.s@indiumsoft.com>
2021-12-14 18:34:23 +0530

    Initial commit for library v1.0

---
+++ toast/src/main/java/com/kotlin/toast/ToastMessage.kt
@@ -0,0 +1,13 @@
+package com.kotlin.toast
+
+import android.content.Context
+import android.widget.Toast
+
+
+public class ToastMessage {
+    companion object {
+        fun s(c: Context?, message: String?) {
+            Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
